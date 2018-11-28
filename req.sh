dbd59960822e243ada3f93dabd0f5e1d8dc100e8
Praneeth Marella <praneethbabumarella@u.boisestate.edu>
2018-11-28 00:18:27 -0700

    Update req.sh

--- req.sh
+++ req.sh
@@ -2,5 +2,7 @@
 
 sudo apt-get install python3-pip
 pip3 install -r requirements.txt
+pip3 install --user --upgrade cozmo
 pip3 install --user cozmo[3dviewer]
+pip3 install --user cozmo[camera]
 sudo apt-get install freeglut3-dev
